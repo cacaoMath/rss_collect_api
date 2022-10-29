851d1ec896710be8fe793168fb9691fa26003fa5
cacaomath <mahiro.1192@gmail.com>
2022-10-29 06:05:37 +0000

    fix: 前処理を追加し、パイプラインにまとめた

--- ml/classifier.py
+++ ml/classifier.py
@@ -1,24 +1,26 @@
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
 import pandas as pd
+
+from util.ml_utils import make_van_list
 
 dataset = pd.read_csv("ml/test_data.csv")
 
+classifier = Pipeline(steps=[
+        # 文字の分析はml_utilの処理に任せるのでanalyzerを指定, 英単語は大小そのまま
+        ("vec", CountVectorizer(lowercase=False, analyzer=lambda x: x)),
+        ("tfidf", TfidfTransformer()),
+        ("Classifier", MultinomialNB())
+    ])
+
 # カテゴリを数値化
 dataset["y"], _ = pd.factorize(dataset["category"])
+dataset["word"] = dataset["word"].apply(make_van_list)
 
-count_vectorizer = CountVectorizer()
-# csr_matrix(疎行列)が返る
-feature_vectors = count_vectorizer.fit_transform(dataset["word"])
-
-clf = MultinomialNB()
-clf.fit(feature_vectors, dataset["y"])
+classifier.fit(dataset["word"], dataset["y"])
 
 pred_x1 = "今秋正式リリースの「Windows 10 バージョン 22H2」にISOイメージファイル"
 pred_x2 = "いまさら聞けないExcelの使い方講座【好評連載中！】"
 
-result = clf.predict(count_vectorizer.transform([pred_x1]))
-print("Windows" if result == 0 else "other")
-
-result = clf.predict(count_vectorizer.transform([pred_x2]))
-print("Windows" if result == 0 else "other")
+print(classifier.predict([make_van_list(pred_x1), make_van_list(pred_x2)]))
