167912ad75f245df2c7f9025beeb21720e0fedfc
cacaomath <mahiro.1192@gmail.com>
2022-11-01 14:22:37 +0000

    feat: docker コンテナ存在確認の処理追加(#10)

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!bin/bash
 MECAB_DIC_PATH=$(echo `mecab-config --dicdir`"/mecab-ipadic-neologd")
 if [ ! -d ./dic ]; then
-mkdir dic
+    mkdir dic
 fi
 if [ -z "$(ls $directory)" ]; then
     cp -r ${MECAB_DIC_PATH=$(echo `mecab-config --dicdir`"/mecab-ipadic-neologd")} ./dic
 fi
 echo "MECAB_DIC_PATH=/dic" > .env
 docker build -t deployimg .
+if [ -n "$(echo `docker ps | grep deployimg`)" ]; then
+    docker stop mycontainer
+    docker rm mycontainer
+fi
 docker run -d -v dic:/dic --name mycontainer -p 8000:8000 deployimg
