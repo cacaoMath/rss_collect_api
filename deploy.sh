b7aa1c4a4d7febee17efd92291afe2fd5da8a86e
cacaomath <mahiro.1192@gmail.com>
2022-11-03 02:46:10 +0000

    fix: デプロイ用のスクリプトの動作が可笑しかったので修正(#10)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!bin/bash
+# mecabの確認・辞書をカレントディレクトリにコピー
 if [ -z "$(echo `mecab-config`)" ]; then
     echo "mecabなどが入っていないのでインストールしてください"
     exit 0
@@ -16,10 +17,15 @@ else
         cp -r ${MECAB_DIC_PATH} ./dic
     fi
 fi
-echo "MECAB_DIC_PATH=/dic/mecab-ipadic-neologd" > .env
-docker build -t deployimg .
-if [ -n "$(echo `docker ps | grep deployimg`)" ]; then
-    docker stop mycontainer
-    docker rm mycontainer
+# 環境変数設定
+touch .env
+if [ -z "grep MECAB_DIC_PATH .env" ]; then
+    echo "MECAB_DIC_PATH=/dic/mecab-ipadic-neologd" >> .env
 fi
-docker run -d --name mycontainer -p 8000:8000 deployimg 
+
+# docker build & run
+docker build -t deployimg .
+if [ -n "$(echo `docker ps -a | grep deployimg`)" ]; then
+    docker stop mycontainer
+fi
+docker run -d --rm --name mycontainer -p 8000:8000 deployimg 
