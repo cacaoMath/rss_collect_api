795b9737521c7f6404313fbb0b635f773fdaac57
cacao math <mahiro.1192@gmail.com>
2022-11-04 00:17:44 +0900

    fix: テストDBを使用するように修正

--- test/test_main.py
+++ test/test_main.py
@@ -2,80 +2,83 @@ from fastapi.testclient import TestClient
 import pandas as pd
 
 from app.main import app
-from app.api.models import LearningData
+from app.api.models import LearningData, Feed
 
 client = TestClient(app)
 
 
-def test_get_feeds_if_feed_is_none(mocker):
-    mocker.patch("app.main.crud.get_feeds", return_value=[])
+def test_get_feeds_if_feed_is_none():
     response = client.get("/feeds")
     assert response.status_code == 200
     assert response.json() == []
 
 
-def test_get_feeds_if_feed_is_one(mocker):
-    mocker.patch(
-        "app.main.crud.get_feeds",
-        return_value=[{
-              "url": "https://example.com/hoge.xml",
-              "description": "hogehoge",
-              "id": 1,
-              "is_active": True
-            }]
+def test_get_feeds_if_feed_is_one(test_db):
+    test_db.add(
+        Feed(
+            id=1,
+            url="https://example.com/hoge.xml",
+            description="hogehoge",
+            is_active=True
+        )
     )
+    test_db.flush()
+    test_db.commit()
     response = client.get("/feeds")
     assert response.status_code == 200
     assert response.json() == [{
-              "url": "https://example.com/hoge.xml",
-              "description": "hogehoge",
-              "id": 1,
-              "is_active": True
+                "url": "https://example.com/hoge.xml",
+                "description": "hogehoge",
+                "id": 1,
+                "is_active": True
             }]
 
 
-def test_get_feeds_if_feed_is_two(mocker):
-    mocker.patch(
-        "app.main.crud.get_feeds",
-        return_value=[{
-              "url": "https://example.com/hoge.xml",
-              "description": "hogehoge",
-              "id": 1,
-              "is_active": True
-            },
-            {
-              "url": "https://example.com/fuga.xml",
-              "description": "fugafuga",
-              "id": 2,
-              "is_active": False
-            }]
-    )
+def test_get_feeds_if_feed_is_two(test_db):
+    data = [
+        Feed(
+            id=1,
+            url="https://example.com/hoge.xml",
+            description="hogehoge",
+            is_active=True
+        ),
+        Feed(
+            id=2,
+            url="https://example.com/fuga.xml",
+            description="fugafuga",
+            is_active=False
+        )
+    ]
+    test_db.add_all(data)
+    test_db.flush()
+    test_db.commit()
     response = client.get("/feeds")
     assert response.status_code == 200
     assert response.json() == [{
-              "url": "https://example.com/hoge.xml",
-              "description": "hogehoge",
-              "id": 1,
-              "is_active": True
+                "url": "https://example.com/hoge.xml",
+                "description": "hogehoge",
+                "id": 1,
+                "is_active": True
             },
             {
-              "url": "https://example.com/fuga.xml",
-              "description": "fugafuga",
-              "id": 2,
-              "is_active": False
+                "url": "https://example.com/fuga.xml",
+                "description": "fugafuga",
+                "id": 2,
+                "is_active": False
             }]
 
 
-def test_get_feed(mocker):
-    mocker.patch(
-        "app.main.crud.get_feed",
-        return_value={
-            "url": "https://example.com/hoge.xml",
-            "description": "hogehoge",
-            "id": 1,
-            "is_active": True
-        }
+def test_get_feed(test_db):
+    test_db.add(
+        Feed(
+            id=1,
+            url="https://example.com/hoge.xml",
+            description="hogehoge",
+            is_active=True
+        )
     )
+    test_db.flush()
+    test_db.commit()
     response = client.get("/feeds/1")
     assert response.status_code == 200
     assert response.json() == {
@@ -92,13 +95,13 @@ def test_classifier_predict_if_learning_data_is_few(test_db):
     response = client.post("/classifier/predict", json={"text": "test"})
     assert response.status_code == 500
     assert response.json() == {
-      "detail": "Learning data is small. Please input more Learning data"
+        "detail": "Learning data is small. Please input more Learning data"
     }
     LearningData(word="piyopiyo", category="kogekoge")
     test_db.commit()
     assert response.status_code == 500
     assert response.json() == {
-      "detail": "Learning data is small. Please input more Learning data"
+        "detail": "Learning data is small. Please input more Learning data"
     }
 
 
