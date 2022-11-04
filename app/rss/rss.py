7ef8c4d3d1a8267889929fba6b5a6c6139f257a5
cacao math <mahiro.1192@gmail.com>
2022-11-04 11:49:38 +0900

    feat: rss_feedのurlから記事一覧を取って返す処理の追加(#11)

--- app/rss/rss.py
+++ app/rss/rss.py
@@ -2,30 +2,32 @@ import json
 import feedparser
 import datetime
 
-from ml.classifier import Classifier
-
-rss_url = "https://rss.itmedia.co.jp/rss/2.0/itmedia_all.xml"
-feed = feedparser.parse(rss_url)
-
-print(feed.entries[0])
-
 
 class Rss():
-    def __init__(self):   
+    def __init__(self):
         self.today = datetime.date.today()
 
-    def get_feed(self, feed_list: list[dict]) -> list:
-        # urlからfeedの内容を取ってくる、取ってきたリストは結合して返す
-        pass
+    # urlからfeedの内容を取ってくる、取ってきたリストは結合して返す
+    def get_feed(self, feed_url_list: list[str]) -> list:
+        if not feed_url_list:
+            return []
+        feed_list = self.__feedparser(feed_url_list)
+        tmp_list = []
+        for feed in feed_list:
+            for entry in feed.entries:
+                tmp_list.append(
+                    {
+                        "title": entry.title,
+                        "link": entry.link,
+                        "summary": entry.summary,
+                        "published": entry.published
+                    }
+                )
+        return tmp_list
 
     def make_articles(self, collect_articles: list[dict]) -> json:
         # ジャンル分けした記事の一覧を整形して返す
         pass
 
-    def __judge_category(self, content: str) -> list:
-        # feedの内容をジャンルに振り分けてリストに出力
-        pass
-
-    def __fetch_rss_urls(self) -> list:
-        # 登録されているrss feedのurl一覧を返す(is_active = Trueのみ)
-        pass
+    def __feedparser(self, url_list: list[str]):
+        return list(map(feedparser.parse, url_list))
