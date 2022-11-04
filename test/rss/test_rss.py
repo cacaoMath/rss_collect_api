e772ab4aa0bdcf7001e62fd13547d5a84f09c5b9
cacao math <mahiro.1192@gmail.com>
2022-11-04 11:50:10 +0900

    feat(test): Rssに追加した処理のテスト(#11)

---
+++ test/rss/test_rss.py
@@ -0,0 +1,43 @@
+from app.rss.rss import Rss
+
+rss = Rss()
+
+
+def test_Rss_get_feed_if_feed_url_is_none():
+    url_list = []
+    assert rss.get_feed(feed_url_list=url_list) == []
+
+
+def test_Rss_get_feed_if_feed_url_is_any():
+    # testではローカルのxmlでfeedparserを使う
+    url_list = [
+        "test/rss/resource/rss_a.xml",
+        "test/rss/resource/rss_a.xml"
+    ]
+    result = rss.get_feed(feed_url_list=url_list)
+    right_result = [
+        {
+            "title": "aaaa",
+            "link": "https://aaa.co.jp/aaa.html",
+            "summary": "hugahuga",
+            "published": "Fri, 04 Nov 2022 09:40:00 +0900"
+        },
+        {
+            "title": "bbbb",
+            "link": "https://bbb.co.jp/bbb.html",
+            "summary": "hogehoge",
+            "published": "Wed, 02 Nov 2022 07:00:00 +0900"
+        },
+        {
+            "title": "cccc",
+            "link": "https://ccc.co.jp/ccc.html",
+            "summary": "kogekoge",
+            "published": "Tue, 01 Nov 2022 11:42:00 +0900"
+        },
+    ]
+    print(right_result + right_result)
+    assert result == (right_result + right_result)
+
+
+def test_Rss_make_articles():
+    pass
