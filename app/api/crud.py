87c2b53a139bfed7d69e93a8f5e53437f0d7831c
cacao math <mahiro.1192@gmail.com>
2022-11-15 00:13:57 +0900

    refactor: npで処理を行うように修正(#11) - 時間を計測した感じpadasで行うよりも早い

--- app/api/crud.py
+++ app/api/crud.py
@@ -85,10 +85,12 @@ def return_rss_articles(db: Session, collect_categories: schemas.CollectCategori
     dataset = make_dataset_from_db(db)
     classifier = Classifier()
     classifier.train(dataset["word"], dataset["category_id"])
-    category = dataset[["category_id", "text"]].drop_duplicates().to_numpy()
+    category = db.query(models.Category).all()
+    category = np.array([[int(elm.id), str(elm.text)] for elm in category])
     # 指定したカテゴリの値分だけ残すようにマスキング
     mask = np.isin(category[:, 1], collect_categories)
-    collect_value_list = category[:, 0][mask]
+    # numpy strのndarrayになっているのでintにする
+    collect_value_list = category[:, 0][mask].astype(np.int64)
     rss = Rss()
     feed_url_list = db.query(models.Feed.url).all()
     feed_url_list = np.array(feed_url_list)[:, 0].tolist()
