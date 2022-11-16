5e8b233ae4f12c91c709a86ae0fcd48c0ea066be
cacao math <mahiro.1192@gmail.com>
2022-11-16 22:52:02 +0900

    feat(test): Rssのmake_articlesのテスト追加(#11)

--- test/rss/test_rss.py
+++ test/rss/test_rss.py
@@ -1,4 +1,7 @@
+import numpy as np
+
 from app.rss.rss import Rss, FeedItem
+from app.ml.classifier import Classifier
 
 rss = Rss()
 
@@ -38,5 +41,68 @@ def test_Rss_get_feed_if_feed_url_is_any():
     assert result == right_result*2, "2つのurl(file)分のFeedItemがかえってくること"
 
 
-def test_Rss_make_articles():
+def test_Rss_make_articles(mocker):
+    feed_list = [
+        FeedItem(
+            title="aaaa",
+            link="https://aaa.co.jp/aaa.html",
+            summary="hugahuga",
+            published="Fri, 04 Nov 2022 09:40:00 +0900"
+        ),
+        FeedItem(
+            title="bbbb",
+            link="https://bbb.co.jp/bbb.html",
+            summary="hogehoge",
+            published="Wed, 02 Nov 2022 07:00:00 +0900"
+        ),
+        FeedItem(
+            title="cccc",
+            link="https://ccc.co.jp/ccc.html",
+            summary="kogekoge",
+            published="Tue, 01 Nov 2022 11:42:00 +0900"
+        ),
+    ]
+    category_calue_list = np.array([1, 2])
+    classifier = Classifier()
+
+    mocker.patch(
+        "app.ml.classifier.Classifier.predict",
+        return_value=np.array([1, 3, 2])
+    )
+    assert rss.make_articles(
+        feed_list=feed_list,
+        category_value_list=category_calue_list,
+        classifier=classifier
+    ) == [
+        FeedItem(
+            title="aaaa",
+            link="https://aaa.co.jp/aaa.html",
+            summary="hugahuga",
+            published="Fri, 04 Nov 2022 09:40:00 +0900"
+        ),
+        FeedItem(
+            title="cccc",
+            link="https://ccc.co.jp/ccc.html",
+            summary="kogekoge",
+            published="Tue, 01 Nov 2022 11:42:00 +0900"
+        ),
+    ]
+    mocker.patch(
+        "app.ml.classifier.Classifier.predict",
+        return_value=np.array([3, 3, 3])
+    )
+    assert rss.make_articles(
+        feed_list=feed_list,
+        category_value_list=category_calue_list,
+        classifier=classifier
+    ) == []
     feed_list = []
+    mocker.patch(
+        "app.ml.classifier.Classifier.predict",
+        return_value=np.array([1, 1, 1])
+    )
+    assert rss.make_articles(
+        feed_list=feed_list,
+        category_value_list=category_calue_list,
+        classifier=classifier
+    ) == []
