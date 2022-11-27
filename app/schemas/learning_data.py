9953c3bb3cb0a0d629b1d677c06331f2fe5936c1
cacao math <mahiro.1192@gmail.com>
2022-11-27 18:52:22 +0900

    fix: バリデーションがミスっていたので修正(#27)

--- app/schemas/learning_data.py
+++ app/schemas/learning_data.py
@@ -14,7 +14,7 @@ class LearningDataCreate(LearningDataBase):
         title="Category name",
         min_length=1,
         max_length=30,
-        regex="^[a-zA-z0-9-].+$"  # categoryはa-z,A-Z,0-9,-のみ使用可とする
+        regex="^[a-zA-Z0-9-]+$"  # categoryはa-z,A-Z,0-9,-のみ使用可とする
     )
 
 
